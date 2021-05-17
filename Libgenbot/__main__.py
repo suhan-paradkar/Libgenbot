7cb90d55ffaffae797cf11dca06219a80d8cac6a
@suhan-paradkar <12suhangp34@gmail.com>
2021-05-17 17:55:20 +0530

    Refine __main__.py

--- Libgenbot/__main__.py
+++ Libgenbot/__main__.py
@@ -9,12 +9,12 @@ from .Scholar import ScholarPapersInfo
 from .Crossref import getPapersInfoFromDOIs
 from .proxy import proxy
 
-def start(query, scholar_results, scholar_pages, dwn_dir, proxy, min_date=None, num_limit=None, num_limit_type=None, filter_jurnal_file=None, restrict=None, DOIs=None, SciHub_URL=None):
+def scholar_start(scholar_query, scholar_results, scholar_pages, dwn_dir, proxy, min_date=None, num_limit=None, num_limit_type=None, filter_jurnal_file=None, restrict=None, DOIs=None, SciHub_URL=None):
 
     to_download = []
     if DOIs==None:
-        print("Query: {}".format(query))
-        to_download = ScholarPapersInfo(query, scholar_pages, restrict, min_date, scholar_results)
+        print("Scholar Query: {}".format(scholar_query))
+        to_download = ScholarPapersInfo(scholar_query, scholar_pages, restrict, min_date, scholar_results)
     else:
         print("Downloading papers from DOIs\n")
         num = 1
@@ -49,38 +49,83 @@ def start(query, scholar_results, scholar_pages, dwn_dir, proxy, min_date=None, 
     Paper.generateBibtex(to_download,dwn_dir+"bibtex.bib")
 
 
+def start(query, results, pages, dwn_dir, proxy, min_date=None, num_limit=None, num_limit_type=None, filter_jurnal_file=None, restrict=None, DOIs=None, Libgen_URL=None):
+
+    to_download = []
+    if DOIs==None:
+        print("Query: {}".format(query))
+        to_download = LibgenPapersInfo(query, pages, restrict, min_date, results)
+    else:
+        print("Downloading papers from DOIs\n")
+        num = 1
+        i = 0
+        while i<len(DOIs):
+            DOI = DOIs[i]
+            print("Searching paper {} of {} with DOI {}".format(num,len(DOIs),DOI))
+            papersInfo = getPapersInfoFromDOIs(DOI, restrict)
+            to_download.append(papersInfo)
+
+            num += 1
+            i +=  1
+
+
+    if restrict!=0 and to_download:
+        if filter_jurnal_file!=None:
+           to_download = filterJurnals(to_download,filter_jurnal_file)
+
+        if min_date!=None:
+            to_download = filter_min_date(to_download,min_date)
+
+        if num_limit_type!=None and num_limit_type==0:
+            to_download.sort(key=lambda x: int(x.sc_year) if x.sc_year!=None else 0, reverse=True)
+
+        if num_limit_type!=None and num_limit_type==1:
+            to_download.sort(key=lambda x: int(x.sc_cites) if x.sc_cites!=None else 0, reverse=True)
+
+
+        downloadPapers(to_download, dwn_dir, num_limit, Libgen_URL)
+
+
+    Paper.generateReport(to_download,dwn_dir+"result.csv")
+    Paper.generateBibtex(to_download,dwn_dir+"bibtex.bib")
+
+
 def main():
     print("""Libgenbot is a simple bot written in python inspired by PyPaperBot""")
-
     parser = argparse.ArgumentParser(description='Libgenbot is python tool to search and download any book using Libgen')
-    parser.add_argument('--query', type=str, default=None, help='Query to make on Google Scholar or Google Scholar page link')
-    parser.add_argument('--doi', type=str, default=None, help='DOI of the paper to download (this option uses only SciHub to download)')
+    parser.add_argument('--query', '-q', type=str, default=None, help='Query to make on Libgen')
+    parser.add_argument('--scholar-query', 'sq', type=str, default=None, help='query to be made on Google Scholar')
+    parser.add_argument('--doi', type=str, default=None, help='DOI of the paper to download (this option uses only Libgen to download)')
     parser.add_argument('--doi-file', type=str, default=None, help='File .txt containing the list of paper\'s DOIs to download')
-    parser.add_argument('--scholar-pages', type=str, help='If given in %%d format, the number of pages to download from the beginning. '
-                                                          'If given in %%d-%%d format, the range of pages (starting from 1) to download (the end is included). '
-                                                          'Each page has a maximum of 10 papers (required for --query)')
+    parser.add_argument('--scholar-pages', '-sp', type=str, help='If given in %%d format, the number of pages to download from the beginning. '
+                                                               'If given in %%d-%%d format, the range of pages (starting from 1) to download (the end is included). '
+                                                               'Each page has a maximum of 10 papers (required for --scholar-query')
+    parser.add_argument('--libgen-pages', '-p', type=str, help='If given in %%d format, the number of pages to download'
+                                                              'If given in %%d-%%d format, the range of pages (starting from 1) to download'
+                                                              'Each page has a maximum of 10 papers (required for --query)')
     parser.add_argument('--dwn-dir', type=str, help='Directory path in which to save the results')
     parser.add_argument('--min-year', default=None, type=int, help='Minimal publication year of the paper to download')
-    parser.add_argument('--max-dwn-year', default=None, type=int, help='Maximum number of papers to download sorted by year')
+    parser.add_argument('--max-dwn-year', default=None, type=int, help='Maximum number of papers to download sorted by year')           
     parser.add_argument('--max-dwn-cites', default=None, type=int, help='Maximum number of papers to download sorted by number of citations')
     parser.add_argument('--journal-filter', default=None, type=str ,help='CSV file path of the journal filter (More info on github)')
     parser.add_argument('--restrict', default=None, type=int ,choices=[0,1], help='0:Download only Bibtex - 1:Down load only papers PDF')
     parser.add_argument('--scihub-mirror', default=None, type=str, help='Mirror for downloading papers from sci-hub. If not set, it is selected automatically')
-    parser.add_argument('--scholar-results', default=10, type=int, choices=[1,2,3,4,5,6,7,8,9,10], help='Downloads the first x results in a scholar page(max=10)')
-    parser.add_argument('--proxy', nargs='*', default=[], help='Use proxychains, provide comma seperated list of proxies to use and please, no spaces')
+    parser.add_argument('--scholar-results', default=10, type=int, choices=[1,2,3,4,5,6,7,8,9,10], help='Downloads the first x results in a scholar page(max=10)') 
+    parser.add_argument('--proxy', nargs='*', default=[], help='Use proxychains, provide comma seperated list of proxies to use and please, no spaces' 
+                                                                'Feature currently in alpha and may contain bugs')
     args = parser.parse_args()
-
+    
     pchain = []
     pchain = args.proxy
-
+    
     proxy(pchain)
 
-    if args.query==None and args.doi_file==None and args.doi==None:
+    if args.query==None and args.doi_file==None and args.doi==None and args.scholar_query==None:
         print("Error, provide at least one of the following arguments: --query or --file")
         sys.exit()
-
-    if (args.query!=None and args.doi_file!=None) or (args.query!=None and args.doi!=None) or (args.doi!=None and args.doi_file!=None):
-        print("Error: Only one option between '--query', '--doi-file' and '--doi' can be used")
+    
+    if (args.query!=None and args.doi_file!=None) or (args.query!=None and args.doi!=None) or (args.doi!=None and args.doi_file!=None) or (args.scholar_query!=None and args.doi.file!=None) or (args.scholar_query!=None and args.doi!=None) or (args.query!=None and args.scholar_query!=None):
+        print("Error: Only one option between '--query', '--scholar-query', '--doi-file' and '--doi' can be used")
         sys.exit()
 
     if args.dwn_dir==None:
@@ -143,7 +188,7 @@ def main():
         max_dwn_type = 1
 
 
-    start(args.query, args.scholar_results, scholar_pages, dwn_dir, proxy, args.min_year , max_dwn, max_dwn_type , args.journal_filter, args.restrict, DOIs, args.scihub_mirror)
+    start(args.query, args.scholar_results, scholar_pages, dwn_dir, proxy, args.min_year , max_dwn, max_dwn_type , args.journal_filter, args.restrict, DOIs, args.libgen_mirror)
 
 if __name__ == "__main__":
     main()
