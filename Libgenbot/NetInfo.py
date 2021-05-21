84895dee7678dcc7dbbfac424497b54b681e6d8e
suhan-paradkar <12suhangp34@gmail.com>
2021-05-21 15:44:18 +0530

    Add libgen URLs repo

--- Libgenbot/NetInfo.py
+++ Libgenbot/NetInfo.py
@@ -2,3 +2,5 @@ class NetInfo:
     SciHub_URL = None
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
     SciHub_URLs_repo = "https://sci-hub.41610.org/"
+    Libgen_URL =None
+    Libgen_URLs_repo = "https://sci-hub.41610.org/library-genesis"
