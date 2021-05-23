c923664fc1f54a33f59fe8c4893c0bfe7c52c579
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-05-23 11:37:25 +0530

    fix pypi license

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
     'Development Status :: 4 - Beta',
     'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering',
-    'License :: OSI Approved :: GPL-3.0 License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
