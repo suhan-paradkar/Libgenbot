bcc624772a44db5bd3863ecfde8221ce6e7e09ff
@suhan-paradkar <12suhangp34@gmail.com>
2021-05-23 11:49:07 +0530

    fix indent errors

--- Libgenbot/Libgen.py
+++ Libgenbot/Libgen.py
@@ -5,17 +5,23 @@ from .HTMLparsers import LibgenParser
 from .Crossref import getPapersInfo
 from .NetInfo import NetInfo
 
-def libgen_requests(libgen_pages, url, restrict, libgen_results=10):                                                   javascript_error = "Sorry, we can't verify that you're not a robot when JavaScript is turned off"
+def libgen_requests(libgen_pages, url, restrict, libgen_results=10): 
+    javascript_error = "Sorry, we can't verify that you're not a robot when JavaScript is turned off"
+    
     to_download = []
+    
     for i in libgen_pages:
         while True:
             res_url = url % (libgen_results * (i - 1))
             html = requests.get(res_url, headers=NetInfo.HEADERS)
             html = html.text
 
-            if javascript_error in html:                                                                                              is_continue = waithIPchange()
+            if javascript_error in html:    
+                is_continue = waithIPchange()
+                
                 if not is_continue:
                     return to_download
+            
             else:
                 break
 
@@ -24,10 +30,13 @@ def libgen_requests(libgen_pages, url, restrict, libgen_results=10):            
 
         if(len(lpapers)>0):
             papersInfo = getPapersInfo(lpapers, url, restrict, libgen_results)
+            
             info_valids = functools.reduce(lambda a,b : a+1 if b.DOI!=None else a, papersInfo, 0)
+            
             print("Papers found on Crossref: {}/{}\n".format(info_valids,len(lpapers)))
 
             to_download.append(papersInfo)
+        
         else:
             print("Paper not found...")
 
@@ -36,6 +45,7 @@ def libgen_requests(libgen_pages, url, restrict, libgen_results=10):            
 def LibgenPapersInfo(lquery, libgen_pages, restrict, min_date=None, libgen_results=10):
 
     url = r"https://libgen.is/scimag/?q="+lquery+"&as_vis=1&as_sdt=1,5&start=%d"
+    
     if min_date!=None:
         url += "&as_ylo="+str(min_date)
 
