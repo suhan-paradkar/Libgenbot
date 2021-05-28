66995d42129dbd172259c1d0c799c0a24a37e72b
@suhan-paradkar <12suhangp34@gmail.com>
2021-05-28 16:15:39 +0530

    squash int-type bug

--- Libgenbot/Libgen.py
+++ Libgenbot/Libgen.py
@@ -13,7 +13,7 @@ def libgen_requests(libgen_pages, genre, url, restrict, libgen_results=25):
     for i in libgen_pages:
         while True:
             k = int(i)
-            res_url = url % str(libgen_results * (k - 1))
+            res_url = str(url) % str(libgen_results * (k - 1))
             html = requests.get(res_url, headers=NetInfo.HEADERS)
             html = html.text
 
