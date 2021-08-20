051eed234af52eaef6c4be312387ceb8c4813a49
Wisest_wizard <76830230+suhan-paradkar@users.noreply.github.com>
2021-08-20 08:12:24 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name = 'Libgenbot',
   packages = setuptools.find_packages(),
-  version = '1.1.0',
+  version = '1.1.1',
   license='GPL-3.0',
   description = 'Libgenbot is a Python tool for downloading articles from Libgen, inspired by PyPaperBot.',
   long_description=long_description,
